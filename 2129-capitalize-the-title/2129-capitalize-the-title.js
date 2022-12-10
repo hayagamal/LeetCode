d604f1069e9befc5477e1b6633c485bc1a6fe6bb
Haya Gamal <50046177+hayagamal@users.noreply.github.com>
2022-12-10 22:09:22 +0200

    Time: 92 ms (64.29%), Space: 44.8 MB (5.59%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.js
+++ 2129-capitalize-the-title/2129-capitalize-the-title.js
@@ -3,11 +3,9 @@
  * @return {string}
  */
 var capitalizeTitle = function(title) {
-   title= title.toLowerCase()
-    title= title.split(" ")
-   title = title.map(t => t.charAt(0).toUpperCase() + t.slice(1).toLowerCase()  
-  )
-  
+
+   title = title.toLowerCase().split(" ").map(t => t.charAt(0).toUpperCase()+ t.slice(1).toLowerCase())
+    
     for(let i =0; i<title.length ;i++){
         if(title[i].length <=2){
           title[i] = title[i].charAt(0).toLowerCase() + title[i].slice(1).toLowerCase()
